9ab3636873eae498e05affe67a87e812722fc87f
andynogamer <antoniomz769@gmail.com>
2026-05-09 11:32:52 -0600

    Se agrega interfaz de busqueda para moviles

--- app/view/shared/header.php
+++ app/view/shared/header.php
@@ -59,8 +59,10 @@ if (session_status() === PHP_SESSION_NONE) { session_start(); }
                 <div class="mobile-sidebar" id="mobileSidebar">
                     <div class="sidebar-header">
                         <h3>Menú</h3>
-                        <button class="btn-search" id="btnSearch"><img src="resources/search.png" alt=""></button>
-                        <button class="btn-close" id="closeMenu">&times;</button>
+                        <div class="container-btn-sidebar">
+                            <button class="btn-search" id="btnSearch"><img src="resources/search.png" alt=""></button>
+                            <button class="btn-close" id="closeMenu">&times;</button>
+                        </div>
                     </div>
                     <div class="sidebar-links">
                         <?php if($_SESSION['user']['tipoUsuario'] == 2): ?>
@@ -82,7 +84,27 @@ if (session_status() === PHP_SESSION_NONE) { session_start(); }
                 </div>
             <?php endif; ?>
         </nav>
+        <div class="overlay-search" id="overlaySearch">
+            <div class="card-overlay-search" id="cardOverlay">
+                <h2>Búsqueda</h2>
+                <div class="container-overlay-input-search">
+                    
+                    
+                    <input type="text" id="terminoBusquedaOverlay" class="busqueda-input-overlay" placeholder="Escribe aquí para buscar...">
+                    
+                    <button id="btnBuscarOverlay" class="busqueda-btn-overlay">Buscar</button>
+                </div>
+                <select id="tipoBusquedaOverlay" class="busqueda-select-overlay">
+                        <option value="categoria">Categoría</option>
+                        <option value="anio">Año</option>
+                        <option value="pais">País Sede</option>
+                        <option value="usuario">Usuario</option>
+                </select>
+            </div>
+        </div>
+
     </div>
+
 </header>
 
 <main class="page-content" id="pageContent">
@@ -91,6 +113,93 @@ if (session_status() === PHP_SESSION_NONE) { session_start(); }
         const btnBuscar = document.getElementById('btnBuscar');
         const selectBuscar = document.getElementById('tipoBusqueda');
         const terminoBusqueda = document.getElementById('terminoBusqueda');
+        const btnSearch = document.getElementById('btnSearch');
+        const overlaySearch = document.getElementById('overlaySearch');
+        const btnBuscarOverlay = document.getElementById('btnBuscarOverlay');
+        const terminoBusquedaOverlay = document.getElementById('terminoBusquedaOverlay');
+        const selectBuscarOverlay = document.getElementById('tipoBusquedaOverlay');
+        const cardOverlay = document.getElementById('cardOverlay');
+        btnSearch.addEventListener('click', () =>{
+            overlaySearch.style.display = 'flex';
+        })
+
+        if(overlaySearch){
+            const closeSearch = () =>{
+                console.log('hola')
+                overlaySearch.style.display = 'none';
+
+            }
+            overlaySearch.addEventListener('click', closeSearch);
+            cardOverlay.addEventListener('click', (e) => {
+
+                e.stopPropagation();
+
+            });
+        }
+
+        btnBuscarOverlay.addEventListener('click', () => {
+            const valorSelectBuscarOverlay = selectBuscarOverlay.value;
+            const valorTerminoBusquedaOverlay = terminoBusquedaOverlay.value;
+            fetch('index.php?action=api_get_search', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ tipoBusqueda: valorSelectBuscarOverlay, termino: valorTerminoBusquedaOverlay })
+            })
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    
+                    const mainPage = document.getElementById('pageContent');
+                    
+                    mainPage.innerHTML = data.data.length ? '<div class="foro-container"><div id="feed-publicaciones" class="feed-section"></div></div>' : '<div class="foro-container"><div id="feed-publicaciones" class="feed-section"><p class="empty">No se han encontrado coincidencias.</p></div></div>';
+                    const feed = document.getElementById('feed-publicaciones');
+                    data.data.forEach(p => {
+                        // Verificamos si es video o imagen basado en el mimeType
+                        let multimediaHtml = '';
+                        if (p.tipoPublicacion === 1) {
+                        multimediaHtml = `<video src="data:video/mp4;base64,${p.multimedia}" class="post-img" controls></video>`;
+                        } else {
+                            multimediaHtml = `<img src="data:image/*;base64,${p.multimedia}" class="post-img">`;
+                        }
+
+                        feed.innerHTML += `
+                            <article class="post-card card">
+                                <div class="post-header">
+                                    <img src="data:image/*;base64,${p.fotoUsuario}" class="user-avatar">
+                                    <div>
+                                        <h3>${p.nombreUsuario} ${p.apellidoUsuario}</h3>
+                                        <span>${p.nombreCategoria} • ${new Date(p.fechaCreacion).toLocaleDateString()}</span>
+                                    </div>
+                                </div>
+                                <p class="post-desc">${p.descripcion}</p>
+                                ${multimediaHtml} 
+                                
+                                <div class="post-footer">
+                                    <button class="btn-like" data-id=${p.idPublicacion}>❤️ <span id="like-count-${p.idPublicacion}">${p.likes}</span> Likes</button>
+                                    <a class="btn-comment" href="index.php?action=publicacion&id=${p.idPublicacion}">💬 ${p.comentarios} Comentarios</a>
+                                </div>
+                                
+                            </article>
+                        `;
+                    });
+                    const sidebarInSearch = document.getElementById('mobileSidebar');
+                    const overlayInSearch = document.getElementById('menuOverlay');
+                    sidebarInSearch.classList.remove('active');
+                    overlayInSearch.classList.remove('active');
+
+                    overlaySearch.style.display = 'none';
+
+
+                    
+                    
+                } else{
+                    
+                    alert("Error: " + data.mensaje);
+                }
+            });
+        })
 
         btnBuscar.addEventListener('click', () => {
             const valorSelectBuscar = selectBuscar.value;
